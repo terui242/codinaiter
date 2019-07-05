e9fe3fafafdebee0f3ab1117b10cebafc45c0cbd
terui <ate2934rui@gmail.com>
2019-07-05 16:38:00 +0900

    html編集

--- www/cgi-bin/form.py
+++ www/cgi-bin/form.py
@@ -3,7 +3,13 @@ import psycopg2
 import random
 form = cgi.FieldStorage()
 
-question_list = ["一年生ですか？","二年生ですか？"]
+conn = psycopg2.connect("dbname=test user=postgres password=password")
+cur = conn.cursor()
+cur.execute("select question_id,question from question")
+
+question_list = []
+# for row in cur:
+#     question_list.append((row[0]))
 
 que = random.choice(question_list)
 
@@ -64,7 +70,7 @@ print("""
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-//ボタンにより数値を取得、コンソールに表示
+//ボタンにより数値を取得、コンソールに表示  
 <script>
  console.log({answer})
 </script>
